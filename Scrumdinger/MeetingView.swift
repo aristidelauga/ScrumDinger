4c8084a2a840f7e201d9e6c4e09a764cc89dbf6a
aristidelauga <aristide.lauga@gmail.com>
2023-03-15 21:08:00 +0100

    DetailView and Edit View in progress

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -10,10 +10,35 @@ import SwiftUI
 struct MeetingView: View {
   var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text("Hello, world!")
+      ProgressView(value: 5, total: 15)
+      HStack {
+        VStack(alignment: .leading) {
+          Text("Seconds elapsed")
+            .font(.caption)
+          Label("300", systemImage: "hourglass.bottomhalf.fill")
+        }
+        Spacer()
+        VStack(alignment: .trailing) {
+          Text("Seconds remaining")
+            .font(.caption)
+          Label("600", systemImage: "hourglass.bottomhalf.fill")
+        }
+      }
+      .accessibilityElement(children: .ignore)
+      .accessibilityLabel("Time remaining")
+      .accessibilityLabel("10 minutes")
+      Circle()
+        .strokeBorder(lineWidth: 24)
+      HStack {
+        Text("Speaker 1 on 3")
+        Spacer()
+        Button {
+          
+        } label: {
+          Image(systemName: "forward.fill")
+        }
+        .accessibilityLabel("Next speaker")
+      }
     }
     .padding()
   }
